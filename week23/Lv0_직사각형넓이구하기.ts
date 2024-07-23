6a99f506039e364f40971065df8822cfe70b1fe1
김다인 <dbslzhs1218@naver.com>
2024-07-24 04:09:53 +0900

    임시 커밋

---
+++ week23/Lv0_직사각형넓이구하기.ts
@@ -0,0 +1,13 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120860
+
+// 2차원 좌표 평면에 변이 축과 평행한 직사각형이 있습니다.
+// 직사각형 네 꼭짓점의 좌표 [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]가 담겨있는 배열 dots가 매개변수로 주어질 때, 직사각형의 넓이를 return
+
+// 변이 축과 평행하려면 x가 다르면 y가 같아야하고, y가 다르면 x가 같아야함
+function solution(
+  dots: [[number, number], [number, number], [number, number], [number, number]]
+): number {
+  const [[x1, y1], [x2, y2], [x3, y3], [x4, y4]] = dots;
+
+  
+}
